2b747b78665d46eb766777d1c21c014e28080f7f
Hongwei Yang <yhw1639@gmail.com>
2026-06-22 09:52:23 -0700

    WIndows warning fix

--- third_party/soralog/include/soralog/util.hpp
+++ third_party/soralog/include/soralog/util.hpp
@@ -43,7 +43,7 @@ namespace soralog::util {
     pthread_setname_np(pthread_self(), buff.data());
 #elif defined(__APPLE__)
     pthread_setname_np(buff.data());
-#else
+#elif !defined(_WIN32) && !defined(__ANDROID__)
 #warning \
     "Function setThreadName() is not implemented for this platform; \
     An auto-generated name will be used instead"
@@ -60,11 +60,13 @@ namespace soralog::util {
   inline void getThreadName(std::array<char, 16> &name) {
     static thread_local std::array<char, 16> thr_name{};
     static thread_local bool initialized = [&] {
-#if (defined(__linux__) && !defined(__ANDROID__)) or defined(__APPLE__)
+#if (defined(__linux__) && !defined(__ANDROID__)) || defined(__APPLE__)
       pthread_getname_np(pthread_self(), thr_name.data(), thr_name.size());
 #else
+#if !defined(_WIN32) && !defined(__ANDROID__)
 #warning \
     "Function getThreadName() is not implemented for this platform; An auto-generated name will be used instead"
+#endif
       auto generated = "Thread#" + std::to_string(getThreadNumber());
       memcpy(thr_name.data(),
              generated.data(),
